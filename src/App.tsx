c3797f297142107e8f80040c01327af5633ec716
Nour_Ashraf <nour.ashraf@webgenstudio.com>
2023-06-18 01:52:16 +0200

    Added personal cv

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,10 @@ import Contact from './Layouts/Contact';
 import { GithubFill, LinkedinBoxFill, LinkedinFill } from 'akar-icons';
 import gsap from 'gsap';
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { Box, CssBaseline, IconButton, ThemeProvider, Typography, useMediaQuery, useTheme } from '@mui/material';
+import { Box, CssBaseline, IconButton, Paper, ThemeProvider, Tooltip, Typography, useMediaQuery, useTheme } from '@mui/material';
 import DrawerComponent from './components/DrawerComponent';
 import { appTheme } from './themes/theme';
+import SimCardDownloadIcon from '@mui/icons-material/SimCardDownload';
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -18,7 +19,7 @@ function App() {
   const nav: any = useRef();
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("md"));
-  
+
   useLayoutEffect(() => {
     let ctx = gsap.context(() => {
       gsap.fromTo(nav.current, { background: 'transparent', boxShadow: '' }, { duration: 0.3, scrollTrigger: { toggleActions: 'play none none reverse', start: 20, scroller: "#main" }, boxShadow: '0 -6px 10px 5px rgba(0,0,0,0.5)', background: "#FFFFFF" })
@@ -29,55 +30,66 @@ function App() {
   return (
     <ThemeProvider theme={appTheme}>
       <CssBaseline enableColorScheme />
-    <main id="main">
-      <div className='nav' ref={nav}>
-        <h1>Nour.dev</h1>
-        
-        {
-          isMobile?
-          <DrawerComponent/>
-          :<nav>
-          <a href="#home"><Typography>Home</Typography></a>
-          <a href="#about"><Typography>About</Typography></a>
-          <a href="#projects"><Typography>Projects</Typography></a>
-          <a href="#contact"><Typography>Contact</Typography></a>
-          <div className='social'>
+      <main id="main">
+        <div className='nav' ref={nav}>
+          <h1>Nour.dev</h1>
+
+          {
+            isMobile ?
+              <DrawerComponent />
+              : <nav>
+                <a href="#home"><Typography>Home</Typography></a>
+                <a href="#about"><Typography>About</Typography></a>
+                <a href="#projects"><Typography>Projects</Typography></a>
+                <a href="#contact"><Typography>Contact</Typography></a>
+                <div className='social'>
+                  <a target={'_blank'} href="https://github.com/NourAshraf01">
+                    <GithubFill color='#666666' />
+                  </a>
+                  <a href="https://www.linkedin.com/in/nour-abdelmaksoud-287393178/" target={'_blank'}>
+                    <LinkedinBoxFill color='#666666' />
+                  </a>
+                </div>
+              </nav>
+          }
+
+
+        </div>
+        <section id='home'>
+          <Home />
+        </section>
+        <section id="about">
+          <About />
+        </section>
+        <section id="projects">
+          <Projects />
+        </section>
+        <section id="contact">
+          <Contact />
+        </section>
+        <Box sx={{ position: 'sticky', bottom: '30px', right: '30px',marginBottom:'10px',marginRight:'10px', display: 'flex', justifyContent: 'flex-end' }}>
+          <Box sx={{ position: 'relative' }}>
+            <Paper sx={{display:'grid',placeItems:"center",borderRadius:'100%', width: '60px', height: '60px'}} elevation={6}>
+              <Tooltip title="Download CV">
+                <IconButton download={true} href={process.env.PUBLIC_URL+'/Nour_Abdelmaksoud_CV.pdf'}>
+                  <SimCardDownloadIcon />
+                </IconButton>
+              </Tooltip>
+            </Paper>
+          </Box>
+        </Box>
+        <footer>
+          <Typography sx={{ textAlign: 'center', fontSize: '18px' }}>Copyright © 2023. All rights are reserved.</Typography>
+          <Box sx={{ display: 'flex', gap: '20px' }}>
             <a target={'_blank'} href="https://github.com/NourAshraf01">
-              <GithubFill color='#666666' />
+              <GithubFill color='white' />
             </a>
             <a href="https://www.linkedin.com/in/nour-abdelmaksoud-287393178/" target={'_blank'}>
-              <LinkedinBoxFill color='#666666' />
+              <LinkedinBoxFill color='white' />
             </a>
-          </div>
-        </nav>
-        }
-        
-
-      </div>
-      <section id='home'>
-        <Home />
-      </section>
-      <section id="about">
-        <About />
-      </section>
-      <section id="projects">
-        <Projects />
-      </section>
-      <section id="contact">
-        <Contact />
-      </section>
-      <footer>
-                <Typography sx={{textAlign:'center',fontSize:'18px'}}>Copyright © 2023. All rights are reserved.</Typography>
-                <Box sx={{display:'flex',gap:'20px'}}>
-                    <a target={'_blank'} href="https://github.com/NourAshraf01">
-                        <GithubFill color='white' />
-                    </a>
-                    <a href="https://www.linkedin.com/in/nour-abdelmaksoud-287393178/" target={'_blank'}>
-                        <LinkedinBoxFill color='white' />
-                    </a>
-                </Box>
-            </footer>
-    </main>
+          </Box>
+        </footer>
+      </main>
     </ThemeProvider>
   );
 }
