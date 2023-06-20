326fb512caacfcdc10a5b92bbc06e7db0c3fa91c
Nour_Ashraf <nour.ashraf@webgenstudio.com>
2023-06-20 18:42:20 +0200

    Fix click event

--- src/components/Skill/index.tsx
+++ src/components/Skill/index.tsx
@@ -23,7 +23,7 @@ const Skill = ({ name, dims }: SkillProps) => {
                                 onOpen={() => setTooltipIsOpen(true)}
                                 onClose={() => setTooltipIsOpen(false)}
                                 >
-            <div className="skill" onPointerDown={(e) => { e.stopPropagation(); e.preventDefault(); setTooltipIsOpen(tooltip => !tooltip) }}>
+            <div className="skill" onClick={(e) => { e.stopPropagation(); e.preventDefault(); setTooltipIsOpen(tooltip => !tooltip) }}>
                 <img draggable={false} width={dims} height={dims} src={profile} alt="" />
             </div>
         </Tooltip>
